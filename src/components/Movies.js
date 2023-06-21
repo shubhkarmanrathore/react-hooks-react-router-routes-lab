be0ba9c7c4a3d8ad211c44ed9208c4fc0b1df8f2
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-06-20 20:06:45 -0700

    adds router lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>Name: {movie.title}</h2>
+        <h2>Runtime: {movie.time} minutes</h2>
+        <ul>
+          {movie.genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  );
 }
 
 export default Movies;
